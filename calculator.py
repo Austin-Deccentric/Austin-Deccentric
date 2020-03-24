2a3dfae22ddb0935a92dbf79bd367d657d311a00
Chukwu Austin <35623912+Austin-Deccentric@users.noreply.github.com>
2020-03-24 22:54:27 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,25 @@
+import math
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def Area(self):
+        return math.pi * self.radius * self.radius
+
+if __name__ == "__main__":
+    while True:
+        r = input("Enter the radius of the circle: ")
+        try:
+             r = float(r)
+        except ValueError :
+            print('Please input a number')
+            continue
+        if r < 1:
+            print("Radius should be positive")
+        else:
+            break
+        
+    obj_1 = Circle(r)
+    print("The area of the circle is %.2f" % obj_1.Area())
+            
